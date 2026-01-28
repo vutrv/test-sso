1b2ea152883f1adf577d5f2b45177979394ffff1
vutrv <vutran.react.node.js@gmail.com>
2026-01-28 15:27:52 +0700

    Add oauth2 authorize dir path

---
+++ src/app/oauth2/authorize/page.tsx
@@ -0,0 +1,11 @@
+import MockSSO from "@/components/MockSSO";
+
+export default function SSO() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <MockSSO />
+      </main>
+    </div>
+  );
+}
