0fd415a8af41a56b4fd83c1cd36d9ddbc88a5e89
vutrv <vutran.react.node.js@gmail.com>
2026-01-28 15:19:18 +0700

    Update nextjs config for sub dir with gh-page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: '/test-sso',
+  assetPrefix: '/test-sso/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
